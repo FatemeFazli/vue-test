ceaa35fe813763d47ed530c8592cf32b0488f411
fateme.fazli <fateme.fazli@baninab.com>
2018-03-06 18:34:02 +0330

    - add material scss example.

--- src/components/Reverse.vue
+++ src/components/Reverse.vue
@@ -1,28 +1,91 @@
 <template>
   <div class="reverse">
-    <p>{{ message }}</p>
-    <button v-on:click="reverseMessage">Reverse Message</button>
+    <div class="page-container">
+      <md-app md-waterfall md-mode="flexible">
+        <md-app-toolbar class="md-large md-primary">
+          <div class="md-toolbar-row">
+            <div class="md-toolbar-section-start">
+              <md-button class="md-icon-button" @click="menuVisible = !menuVisible">
+                <md-icon>menu</md-icon>
+              </md-button>
+            </div>
+
+            <div class="md-toolbar-section-end">
+              <md-button class="md-icon-button">
+                <md-icon>more_vert</md-icon>
+              </md-button>
+            </div>
+          </div>
+
+          <div class="md-toolbar-row md-toolbar-offset">
+            <span class="md-display-1">My Title</span>
+          </div>
+        </md-app-toolbar>
+
+        <md-app-drawer :md-active.sync="menuVisible">
+          <md-toolbar class="md-transparent" md-elevation="0">Navigation</md-toolbar>
+
+          <md-list>
+            <md-list-item>
+              <md-icon>move_to_inbox</md-icon>
+              <span class="md-list-item-text">Inbox</span>
+            </md-list-item>
+
+            <md-list-item>
+              <md-icon>send</md-icon>
+              <span class="md-list-item-text">Sent Mail</span>
+            </md-list-item>
+
+            <md-list-item>
+              <md-icon>delete</md-icon>
+              <span class="md-list-item-text">Trash</span>
+            </md-list-item>
+
+            <md-list-item>
+              <md-icon>error</md-icon>
+              <span class="md-list-item-text">Spam</span>
+            </md-list-item>
+          </md-list>
+        </md-app-drawer>
+
+        <md-app-content>
+          <p>Hello World</p>
+          <p>Hello World</p>
+          <p>Hello World</p>
+          <p>Hello World</p>
+          <p>Hello World</p>
+          <p>Hello World</p>
+          <p>Hello World</p>
+          <p>Hello World</p>
+          <p>Hello World</p>
+          <p>Hello World</p>
+          <p>Hello World</p>
+          <p>Hello World</p>
+          <p>Hello World</p>
+          <p>Hello World</p>
+          <p>Hello World</p>
+          <p>Hello World</p>
+          <p>Hello World</p>
+          </md-app-content>
+      </md-app>
+    </div>
+
+    <!--<p>{{ message }}</p>-->
+    <!--<button v-on:click="reverseMessage">Reverse Message</button>-->
   </div>
 </template>
 
 <script>
 export default {
   name: 'Reverse',
-  data () {
-    return {
-      message: 'bye Vue.js!'
-    }
-  },
-  methods: {
-    reverseMessage: function () {
-      this.message = this.message.split('').reverse().join('')
-    }
-  }
+  data: () => ({
+    menuVisible: false
+  }),
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+
+<style lang="scss" scoped>
 h1, h2 {
   font-weight: normal;
 }
@@ -37,4 +100,18 @@ li {
 a {
   color: #42b983;
 }
+.md-app {
+  max-height: 400px;
+  border: 1px solid rgba(#000, .12);
+}
+
+.md-app-toolbar {
+  height: 196px;
+}
+
+.md-drawer {
+     width: 230px;
+     max-width: calc(100vw - 125px);
+}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
 
 Vue.config.productionTip = false
+Vue.use(VueMaterial)
 
 new Vue({
+
   el: '#app',
   router,
   components: { App },

121ccba69d1321f16cfa48c5fc8f320094ea725b
fateme.fazli <fateme.fazli@baninab.com>
2018-03-06 16:57:18 +0330

    - add simple reverse function .

---
+++ src/components/Reverse.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="reverse">
+    <p>{{ message }}</p>
+    <button v-on:click="reverseMessage">Reverse Message</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Reverse',
+  data () {
+    return {
+      message: 'bye Vue.js!'
+    }
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
